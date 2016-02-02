437dfb60a2f4cf5d6127a91d26d1c91400b21152
andrewhmorrison93 <andrewhmorrison93@gmail.com>
2016-02-02 12:40:16 -0500

    more frontend

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -6,49 +6,26 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('HomeController', function($scope, $firebaseObject, $firebaseArray, $http, $state) {
-  // var ref = new Firebase('https://popping-heat-9764.firebaseio.com/');
-
-  var ref = new Firebase('https://character-test.firebaseio.com/Andrew/Decision/choice');
-  var decisionRef = new Firebase('https://character-test.firebaseio.com/Andrew/Decision/answered');
-
-  // Download the data into a local object
-  // var syncObject = $firebaseObject(ref);
-  // P.S. ^ You could also do something like this:
-  // $firebaseObject(ref.child('profiles').child('physicsmarie'));
-
-  // var syncObject = $firebaseObject(ref);
-
-
-  // Synchronize the object with three-way binding
-  // syncObject.$bindTo($scope, "data");
-  // The database will now have a key 'text' with associated value of
-  // whatever is typed into the input array in the home view.
-
-  // syncObject.$bindTo($scope, "data");
-
-  //an array of all the choices
-  $scope.choices = $firebaseArray(ref);
-  console.log($scope.choices);
-
-  //access to the answered key
-  $scope.answered = $firebaseObject(decisionRef);
-  console.log($scope.answered);
-
-  //three way data binding so that when an answer is chosen, it changes answered to true
-  $scope.answered.$bindTo($scope, "answered");
-
-  //the function that is called when a choice is chosen, so that the corresponding reaction function can be called
-  $scope.choose = function(choice) {
-    console.log(choice.$value);
-    $scope.answered.$value = true;
-    return choice.$value;
-  }
-
+app.controller('HomeController', function($scope, $firebaseObject, $firebaseArray, HomeFactory, $state) {
   $scope.build = function(){
-    $http.get('/api/game/build').then(function(response){
-      console.log(response.data);
-      $state.go('register');
-    })
+    console.log("hi");
+      return HomeFactory.buildGame()
+      .then(function(){
+        $state.go('register');
+      })
+      .then(null,console.error);
   }
+
 });
+
+app.factory('HomeFactory', function($http) {
+  return {
+    buildGame : function () {
+      return $http.get('api/game/build')
+      .then(function(response){
+        console.log("response is", response);
+        return response.data;
+      })
+    }
+  }
+})

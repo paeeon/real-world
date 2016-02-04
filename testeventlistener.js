62e0e1355a7c58d9aa147ed8d5a1ffcd8c8526b9
Michael DeNatale <mikedeeno84@gmail.com>
2016-02-04 15:13:56 -0500

    implement voting backend listener

--- testeventlistener.js
+++ testeventlistener.js
@@ -2,21 +2,21 @@ var Firebase = require('firebase');
 
 var myFirebaseRef = new Firebase("https://flickering-inferno-4436.firebaseio.com/");
 
-gameId = "K9cpfp9AxxsXHgEgFl_"
+gameId = "-K9hE8L_Y2NAxvi8x06R"
 testEvent = {
-	eventId: "56b0eb7784cfdcefcb8a75d4",
+	eventId: "56b280b7e8813e79132b5612",
 	choice: "hi!",
-	willTrigger: "56b0eb7784cfdcefcb8a75d3"
+	willTrigger: "56b280b7e8813e79132b55ee"
 }
-console.log(myFirebaseRef.child(gameId))
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
-myFirebaseRef.child(gameId).child('votes').child('56b0eb7784cfdcefcb8a75d4').push(testEvent);
+
+// myFirebaseRef.child('games').child(gameId).child('votes').child('56b280b7e8813e79132b55ef').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('56b280b7e8813e79132b55ff').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('56b280b7e8813e79132b560a').push(testEvent);
+myFirebaseRef.child('games').child(gameId).child('votes').child('56b280b7e8813e79132b5612').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('gff').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('a').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('c').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('d').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('f').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('v').push(testEvent);
+// myFirebaseRef.child('games').child(gameId).child('votes').child('bb').push(testEvent);

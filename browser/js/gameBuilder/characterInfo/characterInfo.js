caa4a9d535fc30c207b9203efa8e234dda9fc823
Nicole <nsiegel2@gmail.com>
2016-02-05 17:27:21 -0500

    add characterInfo state and controller

---
+++ browser/js/gameBuilder/characterInfo/characterInfo.js
@@ -0,0 +1,27 @@
+app.config(function ($stateProvider) {
+  $stateProvider.state('buildCharacter', {
+    url: '/game-builder/:gameId/character',
+    templateUrl: 'js/gameBuilder/characterInfo/characterInfo.html',
+    controller: 'buildCharacter'
+  });
+});
+
+app.controller('buildCharacter', function($scope, $q, $state, $stateParams, gameBuildFactory) {
+  var gameId = $stateParams.gameId;
+  $scope.submitCharacters = function() {
+    // create characters
+    var charPromises = $scope.characters.map(function(character) {
+      return gameBuildFactory.createCharacter(character);
+    });
+    return $q.all(charPromises)
+    .then(function(resolvedCharacters) {
+      // use created characters to push ids onto game.characters
+      var characterPush = resolvedCharacters.map(function(character) {
+        return gameBuildFactory.pushCharacterToGame(gameId, character._id);
+      });
+      return $q.all(characterPush);
+    }).then(function(resolved) {
+      state.go('buildEvent', {gameId: gameId});
+    }).then(null, console.log);
+  };
+});

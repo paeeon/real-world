7e9b0f2ae6e6939e880ff168f0126fe8d442c310
Nicole <nsiegel2@gmail.com>
2016-02-06 11:45:37 -0500

    remove require from title prop in event schema

--- server/db/models/event.js
+++ server/db/models/event.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
   title: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   triggeredBy: {
     type: String,

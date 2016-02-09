c2f892a86e4137fa450c68d13a0154893db36a2c
Nicole <nsiegel2@gmail.com>
2016-02-09 12:14:45 -0500

    add fn to update nested events in game build factory

--- browser/js/common/factories/gameBuildFactory.js
+++ browser/js/common/factories/gameBuildFactory.js
@@ -56,6 +56,47 @@ app.factory('gameBuildFactory', function($http) {
           oneEvent.columns = [[]];
         }
         return oneEvent;
+    },
+    saveNestedEvents: function(events) {
+      var gameBuildFactory = this;
+      // console.log($scope.models.dropzones.A);
+      return events.map(function(eventNest) {
+        // check to see if the event has any events in columns
+        // if it doesnt update event
+        if (!eventNest.columns[0][0]) {
+          console.log('updating ', eventNest._id);
+          if (!eventNest.triggeredBy) {
+            eventNest.triggeredBy = 'time';
+          }
+          return gameBuildFactory.updateEvent(eventNest);
+        }
+        else {
+          // if the event is a text event
+          // save the inner event to the willTrigger of that event
+          if (eventNest.type === 'text') {
+            console.log('text event with willTrigger');
+            eventNest.willTrigger = eventNest.columns[0][0]._id;
+            eventNest.columns[0][0].triggeredBy = 'event';
+            gameBuildFactory.saveNestedEvents(eventNest.columns[0]);
+          }
+          // if the event is a choice event
+          else if (eventNest.type === 'choice') {
+            console.log('choice event with choices that willTrigger');
+            // loop through the columns
+            // (for choice events columns are the choices)
+            eventNest.columns[0].forEach(function(choice, i) {
+              // if any of the choices triggers an event
+              // console.log(choice.columns[0][0])
+              if (choice.columns[0][0]) {
+                eventNest.decision.choices[i].willTrigger = choice.columns[0][0]._id;
+                choice.columns[0][0].triggeredBy = 'event';
+                gameBuildFactory.saveNestedEvents(choice.columns[0]);
+              }
+            });
+          }
+          return gameBuildFactory.updateEvent(eventNest);
+        }
+      });
     }
   };
 });
